29da0a94e0472c77a26385e3d4d16bffac844bb3
melked <ce.melikedemirtas@gmail.com>
2025-07-13 18:05:55 +0300

    fix: PackageModel.py updated

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -1,8 +1,7 @@
-from pydantic import Field, validator, root_validator
+from pydantic import Field, validator
 from typing import List, Optional, Union, Literal
-from sdks.novavision.src.base.model import (
-    Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
-)
+from sdks.novavision.src.base.model import Package, Image, Inputs, Configs, Outputs, Response, Request, Output, Input, Config
+
 
 
 class InputImage(Input):
@@ -11,13 +10,12 @@ class InputImage(Input):
     type: str = "object"
 
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
+    def set_type_based_on_value(cls, value, values):
         value = values.get('value')
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return "object"
 
     class Config:
         title = "Image"
@@ -29,174 +27,211 @@ class OutputImage(Output):
     type: str = "object"
 
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
+    def set_type_based_on_value(cls, value, values):
         value = values.get('value')
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return "object"
 
     class Config:
         title = "Image"
 
-# ------------------------------
-# Gray Executor
-# ------------------------------
 
 class KeepSideFalse(Config):
     name: Literal["False"] = "False"
     value: Literal[False] = False
     type: Literal["bool"] = "bool"
     field: Literal["option"] = "option"
+
     class Config:
         title = "Disable"
+
 
 class KeepSideTrue(Config):
     name: Literal["True"] = "True"
     value: Literal[True] = True
     type: Literal["bool"] = "bool"
     field: Literal["option"] = "option"
+
     class Config:
         title = "Enable"
+
 
 class KeepSideBBox(Config):
     name: Literal["KeepSide"] = "KeepSide"
     value: Union[KeepSideTrue, KeepSideFalse]
     type: Literal["object"] = "object"
     field: Literal["dropdownlist"] = "dropdownlist"
+
     class Config:
         title = "Keep Sides"
 
+
 class Degree(Config):
     name: Literal["Degree"] = "Degree"
-    value: int = Field(ge=-359, le=359, default=0)
+    value: int = Field(ge=-359.0, le=359.0, default=0)
     type: Literal["number"] = "number"
     field: Literal["textInput"] = "textInput"
+
     class Config:
-        title = "Angle"
+        title = "Angleeeee"
+
 
 class GrayInputs(Inputs):
     inputImage: InputImage
+
 
 class GrayConfigs(Configs):
     degree: Degree
     drawBBox: KeepSideBBox
 
+
 class GrayRequest(Request):
     inputs: Optional[GrayInputs]
     configs: GrayConfigs
 
+    class Config:
+        json_schema_extra = {
+            "target": "configs"
+        }
+
+
 class GrayOutputs(Outputs):
     outputImage: OutputImage
 
+
 class GrayResponse(Response):
     outputs: GrayOutputs
+
 
 class Gray(Config):
     name: Literal["Gray"] = "Gray"
     value: Union[GrayRequest, GrayResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
+
     class Config:
         title = "GrayExecutor"
+        json_schema_extra = {
+            "target": {
+                "value": 0
+            }
+        }
 
+
+# === Compare Executor Input/Output ===
 
 class InputImage1(Input):
     name: Literal["inputImage1"] = "inputImage1"
     value: Union[List[Image], Image]
     type: str = "object"
+
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
-        value = values.get('value')
+    def set_type_based_on_value(cls, value, values):
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return "object"
+
 
 class InputImage2(Input):
     name: Literal["inputImage2"] = "inputImage2"
     value: Union[List[Image], Image]
     type: str = "object"
+
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
-        value = values.get('value')
+    def set_type_based_on_value(cls, value, values):
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return "object"
 
-class CompareInputs(Inputs):
-    inputImage1: InputImage1
-    inputImage2: InputImage2
-
-class CompareConfigs(Configs):
-    drawBBox: KeepSideBBox
-
-class CompareRequest(Request):
-    inputs: Optional[CompareInputs]
-    configs: CompareConfigs
 
 class SimilarityScore(Output):
     name: Literal["similarityScore"] = "similarityScore"
     value: float
     type: Literal["number"] = "number"
 
+
 class DiffImage(Output):
     name: Literal["diffImage"] = "diffImage"
     value: Union[List[Image], Image]
     type: str = "object"
+
     @validator("type", pre=True, always=True)
-    def set_type_based_on_value(cls, v, values):
-        value = values.get('value')
+    def set_type_based_on_value(cls, value, values):
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return "object"
+
+
+class CompareInputs(Inputs):
+    inputImage1: InputImage1
+    inputImage2: InputImage2
+
+
+class CompareConfigs(Configs):
+    pass
+
 
 class CompareOutputs(Outputs):
     similarityScore: SimilarityScore
     diffImage: DiffImage
 
+
+class CompareRequest(Request):
+    inputs: Optional[CompareInputs]
+    configs: Optional[CompareConfigs]
+
+    class Config:
+        json_schema_extra = {
+            "target": "inputs"
+        }
+
+
 class CompareResponse(Response):
     outputs: CompareOutputs
+
 
 class Compare(Config):
     name: Literal["Compare"] = "Compare"
     value: Union[CompareRequest, CompareResponse]
     type: Literal["object"] = "object"
     field: Literal["option"] = "option"
+
     class Config:
         title = "CompareExecutor"
+        json_schema_extra = {
+            "target": {
+                "value": 0
+            }
+        }
 
 
 
-class ExecutorSelector(Config):
-    name: Literal["ExecutorSelector"] = "ExecutorSelector"
-    selected_executor: Literal["Gray", "Compare"]
-    executor: Union[Gray, Compare] = Field(..., discriminator="name")
+class ConfigExecutor(Config):
+    name: Literal["ConfigExecutor"] = "ConfigExecutor"
+    value: Union[Gray, Compare]
+    type: Literal["executor"] = "executor"
+    field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
     class Config:
-        title = "Executor Selector"
-
-    @root_validator(pre=True)
-    def check_executor_matches_selection(cls, values):
-        sel = values.get("selected_executor")
-        exe = values.get("executor")
-        if sel == "Gray" and exe.get("name") != "Gray":
-            raise ValueError("selected_executor 'Gray' seçildi ama executor Gray değil")
-        if sel == "Compare" and exe.get("name") != "Compare":
-            raise ValueError("selected_executor 'Compare' seçildi ama executor Compare değil")
-        return values
+        title = "Task"
+        json_schema_extra = {
+            "target": "value"
+        }
 
 
 class PackageConfigs(Configs):
-    executor_selector: ExecutorSelector
+    executor: ConfigExecutor
+
 
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
-    name: Literal["Gray"] = "Gray"
+    name: Literal["Gray"] = "Gray"  # İstersen "MultiExecutor" vs olarak değiştirilebilir
