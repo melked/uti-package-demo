04ff72a435041594253289f9876a634c5eca7692
melked <ce.melikedemirtas@gmail.com>
2025-07-14 17:31:04 +0300

    fix: Compare.py update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,36 +1,43 @@
-from copy import deepcopy
 from components.GrayCompare.src.models.PackageModel import (
-    Compare, CompareOutputs, CompareResponse, DiffImage,
+    Compare, CompareOutputs, CompareResponse,
     Gray, GrayOutputs, GrayResponse,
+    OutputFirstImage, OutputSecondImage, DiffImage,
     ConfigExecutor, PackageConfigs, PackageModel
 )
 from sdks.novavision.src.helper.package import PackageHelper
 
+
 def build_response(context, diff_image=None, second_image=None, is_compare=False):
     """
-    context: executor objesi
-    diff_image: Image objesi (compare için diff resmi)
-    second_image: Image objesi (compare için ikinci input resmi)
-    is_compare: bool, True ise compare, False ise gray output oluşturulur
+    :param context: executor objesi (Gray ya da Compare sınıfı)
+    :param diff_image: Image objesi (Compare için oluşturulan diff görüntüsü)
+    :param second_image: Image objesi (Compare için ikinci input image)
+    :param is_compare: Compare işlemi mi? (True/False)
+    :return: PackageModel
     """
 
     if is_compare:
+        # Compare için iki output oluşturuluyor
+        diff_output = DiffImage(value=diff_image)
+        second_output = OutputSecondImage(value=second_image)
 
-        diff_img_output = DiffImage(value=diff_image)
         compare_outputs = CompareOutputs(
-            outputFirstImage=diff_img_output,
-            outputSecondImage=second_image
+            outputFirstImage=diff_output,
+            outputSecondImage=second_output
         )
+
         compare_response = CompareResponse(outputs=compare_outputs)
-        compare = Compare(value=compare_response)
-        config_executor = ConfigExecutor(value=compare)
+        executor_config = Compare(value=compare_response)
 
     else:
-        output_image = GrayOutputs(outputFirstImage=context.image)
-        gray_response = GrayResponse(outputs=output_image)
-        gray = Gray(value=gray_response)
-        config_executor = ConfigExecutor(value=gray)
+        # Gray işlemi için tek output
+        output = OutputFirstImage(value=context.image)
+        gray_outputs = GrayOutputs(outputFirstImage=output)
+        gray_response = GrayResponse(outputs=gray_outputs)
+        executor_config = Gray(value=gray_response)
 
+    # Executor’ü package’a sarmala
+    config_executor = ConfigExecutor(value=executor_config)
     package_configs = PackageConfigs(executor=config_executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=package_configs)
     package_model = package.build_model(context)
