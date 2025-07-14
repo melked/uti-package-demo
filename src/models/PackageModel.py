68ba1c1c24c08209197853c81a39efee8f8cf7b6
melked <ce.melikedemirtas@gmail.com>
2025-07-14 13:14:13 +0300

    fix: PackageModel.py update

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -6,8 +6,6 @@ from sdks.novavision.src.base.model import (
 )
 
 
-
-
 class InputFirstImage(Input):
     name: Literal["inputFirstImage"] = "inputFirstImage"
     value: Union[List[Image], Image]
@@ -15,12 +13,11 @@ class InputFirstImage(Input):
 
     @validator("type", pre=True, always=True)
     def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return value
 
     class Config:
         title = "Image"
@@ -33,12 +30,11 @@ class InputSecondImage(Input):
 
     @validator("type", pre=True, always=True)
     def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return value
 
     class Config:
         title = "Image"
@@ -51,12 +47,11 @@ class OutputFirstImage(Output):
 
     @validator("type", pre=True, always=True)
     def set_type_based_on_value(cls, value, values):
-        value = values.get('value')
+        value = values.get("value")
         if isinstance(value, Image):
             return "object"
         elif isinstance(value, list):
             return "list"
-        return value
 
     class Config:
         title = "Image"
@@ -210,7 +205,6 @@ class Compare(Config):
 
 
 
-
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
     value: Union[Gray, Compare]
@@ -224,49 +218,7 @@ class ConfigExecutor(Config):
 class PackageConfigs(Configs):
     executor: ConfigExecutor
 
-
 class PackageModel(Package):
     configs: PackageConfigs
     type: Literal["component"] = "component"
     name: Literal["GrayCompare"] = "GrayCompare"
-
-
-
-from components.Gray.src.models.PackageModel import (
-    PackageModel, PackageConfigs, ConfigExecutor,
-    Gray, GrayResponse, GrayOutputs, OutputImage,
-    Compare, CompareResponse, CompareOutputs, SimilarityScore, DiffImage
-)
-from sdks.novavision.src.helper.package import PackageHelper
-
-
-def build_response(context, similarity_score=None, diff_image=None, is_compare=False):
-    """
-    context: component context, context.image en azından olmalı
-    similarity_score: float, sadece compare için
-    diff_image: Image objesi, sadece compare için
-    is_compare: bool, False ise Gray, True ise Compare output oluşturulur
-    """
-
-    if is_compare:
-
-        sim_score = SimilarityScore(value=similarity_score if similarity_score is not None else 0.0)
-        diff_img_output = DiffImage(value=diff_image)
-        compare_outputs = CompareOutputs(similarityScore=sim_score, diffImage=diff_img_output)
-        compare_response = CompareResponse(outputs=compare_outputs)
-        compare = Compare(value=compare_response)
-        config_executor = ConfigExecutor(value=compare)
-
-    else:
-
-        output_image = OutputImage(value=context.image)
-        gray_outputs = GrayOutputs(outputImage=output_image)
-        gray_response = GrayResponse(outputs=gray_outputs)
-        gray = Gray(value=gray_response)
-        config_executor = ConfigExecutor(value=gray)
-
-    package_configs = PackageConfigs(executor=config_executor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=package_configs)
-    package_model = package.build_model(context)
-
-    return package_model
