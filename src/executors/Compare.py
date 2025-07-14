15798758dad254106d71b0bb608e716f5f581488
melked <ce.melikedemirtas@gmail.com>
2025-07-14 17:43:36 +0300

    fix: Compare.py update

--- src/executors/Compare.py
+++ src/executors/Compare.py
@@ -66,7 +66,6 @@ class Compare(Component):
         second_image_obj = deepcopy(img2)
         image2_output = Image.set_frame(img=second_image_obj, package_uID=self.uID, redis_db=self.redis_db)
 
-
         self.similarityScore = similarity
         self.image = diff_img
 
